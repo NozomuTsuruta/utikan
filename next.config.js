1a19de768dcc74a000af09cde902eb09eb61909c
NozomuTsuruta <tuno.0806@icloud.com>
2021-01-17 00:25:40 +0900

    supabase initialized

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    SUPABASE_URL: "https://qgqsivkkzmazfdvhuuiq.supabase.co",
+    SUPABASE_KEY:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxMDgwODE5NiwiZXhwIjoxOTI2Mzg0MTk2fQ.SXtQ42J_SdRCjJZdB-tso-aAQC-j_HaOmLa9WeiR6-g",
+  },
+};

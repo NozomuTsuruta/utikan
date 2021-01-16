478d1bce5338b34331b031facfed98a8131a6485
NozomuTsuruta <tuno.0806@icloud.com>
2021-01-16 17:38:54 +0900

    tailwind設定追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
-import App, { AppProps } from 'next/app'
-import { appWithFleurContext, FleurAppContext } from '../lib/fleur'
+import App, { AppProps } from "next/app";
+import { appWithFleurContext, FleurAppContext } from "../lib/fleur";
+import "../styles/tailwind.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
 MyApp.getInitialProps = async (appContext: FleurAppContext) => {
   // calls page's `getInitialProps` and fills `appProps.pageProps`
-  const appProps = await App.getInitialProps(appContext)
+  const appProps = await App.getInitialProps(appContext);
 
-  return { ...appProps }
-}
+  return { ...appProps };
+};
 
-export default appWithFleurContext(MyApp)
+export default appWithFleurContext(MyApp);

b1d6335cb4ca00a997c390de7300b64b2486326b
NozomuTsuruta <tuno.0806@icloud.com>
2021-01-17 17:33:48 +0900

    サインアップ実装

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -9,14 +9,15 @@ type IForm = {
 export default function signin() {
   const { register, handleSubmit } = useForm<IForm>();
 
-  const submit = ({ email, password }: IForm) => {
-    supabase.auth.signIn({ email, password });
+  const submit = async ({ email, password }: IForm) => {
+    const { user, error } = await supabase.auth.signIn({ email, password });
+    console.log(user);
   };
 
   return (
     <form onSubmit={handleSubmit(submit)}>
-      <input type="email" name="email" />
-      <input type="password" name="password" />
+      <input type="email" name="email" ref={register} />
+      <input type="password" name="password" ref={register} />
       <button type="submit">サインイン</button>
     </form>
   );
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,3 +1,28 @@
+import { useForm } from "react-hook-form";
+import { supabase } from "../util/supabase";
+
+type IForm = {
+  name: string;
+  email: string;
+  password: string;
+  passwordConf: string;
+};
+
 export default function signup() {
-  return <div></div>;
+  const { register, handleSubmit } = useForm<IForm>();
+
+  const submit = async ({ email, password }: IForm) => {
+    const { user, error } = await supabase.auth.signUp({ email, password });
+    console.log(user);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(submit)}>
+      <input type="text" name="name" ref={register} />
+      <input type="email" name="email" ref={register} />
+      <input type="password" name="password" ref={register} />
+      <input type="password" name="passwordConf" ref={register} />
+      <button type="submit">サインイン</button>
+    </form>
+  );
 }

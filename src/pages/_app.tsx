2ceac4a3d22aaa21f3409d359b630b95252bb132
NozomuTsuruta <tuno.0806@icloud.com>
2021-01-17 17:28:51 +0900

    サインイン実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,8 @@
 import App, { AppProps } from "next/app";
 import { appWithFleurContext, FleurAppContext } from "../lib/fleur";
 import "../styles/tailwind.css";
-import { createClient } from "@supabase/supabase-js";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_KEY
-  );
   return <Component {...pageProps} />;
 }
 
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,3 +1,23 @@
+import { useForm } from "react-hook-form";
+import { supabase } from "../util/supabase";
+
+type IForm = {
+  email: string;
+  password: string;
+};
+
 export default function signin() {
-  return <div></div>;
+  const { register, handleSubmit } = useForm<IForm>();
+
+  const submit = ({ email, password }: IForm) => {
+    supabase.auth.signIn({ email, password });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(submit)}>
+      <input type="email" name="email" />
+      <input type="password" name="password" />
+      <button type="submit">サインイン</button>
+    </form>
+  );
 }

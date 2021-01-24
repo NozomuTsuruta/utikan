94c22a70b4a334f03a5720274aecd2a4acc2bb93
NozomuTsuruta <tuno.0806@icloud.com>
2021-01-24 17:52:48 +0900

    config削除

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-    SUPABASE_URL: "https://jnkmpzdgrwmpbhbkfrhs.supabase.co",
-    SUPABASE_KEY:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxMTM3MzcyMSwiZXhwIjoxOTI2OTQ5NzIxfQ.bbTXO1AuEfTJJCyutoKVxO7NT0ViYhnTAyvoPsQHr4A",
-  },
-};
